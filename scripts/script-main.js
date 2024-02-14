a7f6a353cf55375d1f38586d41012e73f29a56c0
LeoGD1 <bridonegd@outlook.com>
2024-02-14 16:03:21 -0600

    plantilla nueva con nuevos datos

---
+++ scripts/script-main.js
@@ -0,0 +1,35 @@
+/* LOADER */
+const pantallaCarga = document.querySelector('.loader');
+const principalNombres = document.querySelector('.principal__nombres');
+const principalSubtitulo = document.querySelector('.principal__subtitulo');
+const principalFecha = document.querySelector('.principal__fecha');
+
+setTimeout(() => {
+    pantallaCarga.classList.add('remove');
+    document.body.style.overflowY = 'scroll';
+    principalNombres.style.animationName = 'entradaInicio';
+    principalSubtitulo.style.animationName = 'entradaInicio';
+    principalFecha.style.animationName = 'entradaInicio';
+},1000);
+
+/* REPRODUCTOR */
+const sound = new Audio('../images/jacarandá.wav');
+
+const play = document.querySelector('.reproductor__play');
+const pausa = document.querySelector('.reproductor__pause');
+
+play.addEventListener('click', () => {
+    sound.play();
+    play.style.display = 'none';
+    pausa.style.display = 'block';
+});
+
+pausa.addEventListener('click', () => {
+    sound.pause();
+    play.style.display = 'block';
+    pausa.style.display = 'none';
+    
+});
+
+
+
